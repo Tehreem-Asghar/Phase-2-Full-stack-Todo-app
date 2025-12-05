aac2b39d4f45bea9ed50c2c9d34c4a4b7f780207
Tehreem-Asghar <tehreemmeo818@gmail.com>
2025-12-05 23:57:24 +0500

    update file  name

--- backend/tests/conftest.py
+++ backend/tests/conftest.py
@@ -1,7 +1,7 @@
 import pytest
 from fastapi.testclient import TestClient
 from sqlmodel import create_engine, Session, SQLModel
-from backend.main import app
+from backend.app import app
 from backend.database import get_session
 
 
